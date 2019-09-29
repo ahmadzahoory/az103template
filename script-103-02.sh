50569144855e0ba5a441c4e9e48bba70feaf675b
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-09-30 00:43:35 +0530

    Add files via upload

---
+++ script-103-02.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get -y update
+apt-get install -y apache2
+sudo apt-get install -y unzip
+systemctl start apache2.service
+cd /var/www/html/
+pwd
+sudo rm index.html
+wget https://raw.github.com/ahmadzahoory/az103template/master/Az-103-M-02-05-Code.zip
+unzip Az-103-M-02-05-Code.zip
