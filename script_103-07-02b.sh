958be16e10931511cb39cf1ae42019e88f941f6c
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-11-05 16:51:09 +0530

    Add files via upload

---
+++ script_103-07-02b.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+apt-get update
+apt-get install -y mysql-server
+mysql
+create database lab103db;
+use lab103db;
+create table product (id int NOT NULL AUTO_INCREMENT, name varchar(255), quantity varchar(255), price varchar(255), PRIMARY KEY (id));
+select * from product;
+insert into product (Name, Quantity, Price) VALUES ('HP Headphone', '03', '1690');
+insert into product (Name, Quantity, Price) VALUES ('HP Mouse', '02', '470');
+insert into product (Name, Quantity, Price) VALUES ('HP Pro Laptop', '01', '59000');
+exit
