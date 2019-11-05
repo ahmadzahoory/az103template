ee26913317a6a1245058d537c7ef04f65241978f
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-11-05 17:20:18 +0530

    Update script-103-07-02b.sh

--- script-103-07-02b.sh
+++ script-103-07-02b.sh
@@ -5,7 +5,6 @@ mysql
 create database lab103db;
 use lab103db;
 create table product (id int NOT NULL AUTO_INCREMENT, name varchar(255), quantity varchar(255), price varchar(255), PRIMARY KEY (id));
-select * from product;
 insert into product (Name, Quantity, Price) VALUES ('HP Headphone', '03', '1690');
 insert into product (Name, Quantity, Price) VALUES ('HP Mouse', '02', '470');
 insert into product (Name, Quantity, Price) VALUES ('HP Pro Laptop', '01', '59000');
