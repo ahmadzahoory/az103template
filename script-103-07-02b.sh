33b3a17ba7b82bf4f38f6b8110b37360878486bb
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-11-07 12:06:06 +0530

    Update script-103-07-02b.sh

--- script-103-07-02b.sh
+++ script-103-07-02b.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-apt-get update
-apt-get install -y mysql-server
+rpm -ivh https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm
+yum -y install mysql-community-server
+systemctl start mysqld
+systemctl enable mysqld
