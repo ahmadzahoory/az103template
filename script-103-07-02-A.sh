efd8beb0392030c215eacf041fad0e240b8b29c5
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-03-10 22:20:19 +0530

    Update and rename script-103-07-01.sh to script-103-07-02-A.sh

--- script-103-07-02-A.sh
+++ script-103-07-02-A.sh
@@ -7,5 +7,5 @@ sudo systemctl enable httpd.service
 sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --reload
 cd /var/www/html/
-sudo wget https://raw.githubusercontent.com/ahmadzahoory/az103template/master/Az-103-M-07-01-Code.zip
-sudo unzip Az-103-M-07-01-Code.zip
+sudo wget https://raw.githubusercontent.com/ahmadzahoory/az103template/master/Az-103-M-07-02-A-Code.zip
+sudo unzip Az-103-M-07-02-A-Code.zip
